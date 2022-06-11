90e02d45dc6811e99d42734d785799ef71c3980d
ngocviet <106135209+ngocviet@users.noreply.github.com>
2022-06-11 11:37:06 +0700

    Add files via upload

--- qlsv.php
+++ qlsv.php
@@ -1,3 +1,28 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['user'])){
+        header("Location: index.php");
+    }
+    $con = new mysqli('localhost','root','','test');
+    if(isset($_POST['themsv'])):
+        $name = htmlspecialchars($_POST["name"]); 
+        $Lop = htmlspecialchars($_POST["Lop"]);
+        $dob = htmlspecialchars($_POST["dob"]);
+        $gender = htmlspecialchars($_POST["gender"]);
+        $subjects = $_POST['subject'];
+        if(is_array($subjects)){
+            $subjects_str = implode(", ", $subjects);
+            $subjects_str = htmlspecialchars($subjects_str);
+        }
+        $sql = sprintf("insert into registration(name,Lop,dob,gender,subject) values('%s','%s','%s','%s','%s')",$name,$Lop,$dob,$gender,$subjects_str);
+        // var_dump($sql);
+        $viet = $con->query($sql);
+        // var_dump($viet);
+        // return;
+        // header('Location: tableqlsv.php?success');
+    endif;
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,126 +30,63 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QLSV</title>
-    <style>
-        label{
-            width: 140px;
-            float: left;
-            display: block;
-        }
-        form>div{
-            margin: 0px 0px 15px 10px;
-        }
-        table{
-            border-collapse: collapse;
-            border: 4px solid black;
-        }
-        input{
-            margin-left: 5px;
-        }
-        th, td {
-            padding: 15px;
-        }
-        button{
-            margin: 5px;
-        }
-    </style>
 </head>
 <body>
-    <?php
-        if(count($_POST) >0){
-            $username=htmlspecialchars($_POST["username"]);
-            $pass=htmlspecialchars($_POST["pass"]);
-            $error=[];
-            if(strlen($username)<7){
-                array_push($error, 'username_length');
-            }
-            if(strlen($pass)<7){
-                array_push($error, 'pass_length');
-            }
-            // if(preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,}$/', $pass)){
-            //     array_push($error, 'pattern');
-            // }
-            if(!($username=='admin123' && $pass=='Admin123@')){
-                array_push($error, 'loginfailed');
-            }
-            $param_str_err=implode('&', $error);
-            if(count($error)>0){
-                header(
-                        'Location: http://localhost:82/C2108G2/index.php?'.$param_str_err,
-                        true,
-                        301
-                );
-            }
-        }
-
-
-
-
-
-        // if(!empty($_POST)){
-        //     if(!$_POST["username"]){
-        //         header('Location: index.php?error=er');
-        //     }
-        //     if(!$_POST["pass"]){
-        //         header('Location: index.php?error=er');
-        //     }
-        //     if($_POST["username"]=='admin123'&&$_POST["pass"]=='1234567'){
-
-        //     }else{
-        //         header('Location: index.php?error=er');
-        //     }
-        // }    
-    ?>
-
-<h2>Quản lý sinh viên</h2>
-    <form method="get" action="">
-        <div><label>Tên sv: </label><input type="text" name="name" autocomplete="off"></div>
+    <form method="post" action="">
+        <div><label>Tên sv: </label><input type="text" name="name" autocomplete="off" required></div>
         <div><label>Năm sinh: </label><input type="date" name="dob"></div>
-        <div><label>Giới tính: </label><input type="radio" name="gender" value="Nam">Nam<input type="radio" name="gender" value="Nữ">Nữ</div>
-        <div><label>Môn học: </label><input type="checkbox" name="subject1" value="Toán">Toán<input type="checkbox" name="subject2" value="Lý">Lý<input type="checkbox" name="subject3" value="Hóa">Hóa</div>
-        <div><label style="color: white;">v</label><input type="button" value="Them Mon"></div>
+        <div><label>Giới tính: </label><input type="radio" name="gender" value="Male">Nam<input type="radio" name="gender" value="Female">Nữ</div>
+        <div><label>Môn học: </label><input type="checkbox" name="subject[]" value="Toán">Toán<input type="checkbox" name="subject[]" value="Lý">Lý<input type="checkbox" name="subject[]" value="Hóa">Hóa</div>
         <div><label>Lớp: </label>
             <select name="Lop">
                 <option value="A">A</option>
                 <option value="B">B</option>
                 <option value="C">C</option>
-            </select><input type="button" value="Them lop">
+            </select>
         </div>
-        <div><button type="submit" name="submit">Thêm sinh viên</button></div>
+        <div><input type="submit" name="themsv" value="Them sinh vien"/></div>
     </form>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>Tên</th>
-                <th>Lớp</th>
-                <th>Năm sinh</th>
-                <th>Giới tính</th>
-                <th>Môn học</th>
-                <th>Cập nhật</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php 
-                if(!empty($_GET)){
-                    if(isset($_GET['submit'])){ ?>
-            <tr>
-                <td><?php echo isset($_GET["name"])?$_GET["name"]:''; ?></td>
-                <td><?php echo isset($_GET["Lop"])?$_GET["Lop"]:''; ?></td>
-                <td><?php echo isset($_GET["dob"])?$_GET["dob"]:''; ?></td>
-                <td><?php echo isset($_GET["gender"])?$_GET["gender"]:''; ?></td>
-                <td><?php echo isset($_GET["subject1"])?$_GET["subject1"].'':'';
-                          echo isset($_GET["subject2"])?$_GET["subject2"].'':'';
-                          echo isset($_GET["subject3"])?$_GET["subject3"].'':'';
-                    // if(isset($_GET["subject[]"])){
-                        // foreach($_GET["subject[]"] as $sub){
-                            // echo $sub.' '; 
-                        // }
-                    // }
-                     ?></td>
-                <td><button>Sửa</button><button>Xóa</button></td>
-            </tr>
-            <?php }} ?>
-        </tbody>
+    <table border="1" style="width: 100%;text-align:center;">
+    <thead>
+        <th>ID</th>
+        <th>Tên</th>
+        <th>Lớp</th>
+        <th>Năm sinh</th>
+        <th>Giới tính</th>
+        <th>Môn học</th>
+        <th>Cập nhật</th>
+    </thead>
+    <tbody>
+        <?php
+            $con = new mysqli('localhost','root','','test');
+            $sqlselec = "SELECT * FROM registration";
+            $check = $con->query($sqlselec);
+            if($check->num_rows > 0){
+                while($rows = $check->fetch_assoc()){
+                    echo'<tr>';
+                        echo'<td>'.$rows['ID'].'</td>';
+                        echo'<td>'.$rows['name'].'</td>';
+                        echo'<td>'.$rows['Lop'].'</td>';
+                        echo'<td>'.$rows['dob'].'</td>';
+                        echo'<td>'.$rows['gender'].'</td>';
+                        echo'<td>'.$rows['subject'].'</td>';
+                        ?>
+                        <td><button onclick="updateSV(<?php echo $rows['ID'] ?>)">Cap nhat</button><button onclick="removeSV(<?php echo $rows['ID']?>)">Xoa</button></td>
+                        <?php
+                    echo'</tr>';
+                }
+            }
+            ?>
+            </tbody>
     </table>
 </body>
+    <script>
+        function removeSV(id){
+        if(confirm("Ban co chac chan muon xoa sinh vien nay khong?")){
+            // window.location.href = "Editt.php?remove&id="+id;
+            let ajax = new XMLHttpRequest();
+            ajax
+        }
+    }
+    </script>
 </html>
